934d789fb84037741d31b3a58b95470a7e5aa54f
acastro-git <53477991+acastro-git@users.noreply.github.com>
2019-07-31 15:56:52 +0200

    More fix

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    result = classes[prediction]
+    result = classes[int(prediction)]
     return JSONResponse({'result': result})
 
 
